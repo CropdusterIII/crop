9aac9d75aef27f095412a7e7b729af7617bcbfc5
Glitch (level-spot-jitterbug) <none>
2021-09-16 18:17:25 +0000

    🐗👨 Checkpoint ./server.js:25363807/358

--- server.js
+++ server.js
@@ -5661,14 +5661,14 @@ var maintainloop = (() => {
   };
   // The NPC function
   let makenpcs = (() => {
-    //let f = (loc, team) => { 
-                //*let o = new Entity(loc);
+    /*let f = (loc, team) => {
+                let o = new Entity(loc);
                     //o.define(Class.baseProtector);
                     //o.team = -team;
                     //o.color = [10, 11, 12, 15][team-1];
-            },
-             //for (let i=1; i<5; i++) 
-                //room['bas' + i].forEach((loc) => { f(loc, i); }); 
+             };
+             for let i=1; i<5; 
+    
     // Return the spawning function
     let bots = [];
     return () => {
